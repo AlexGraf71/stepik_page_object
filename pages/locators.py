d374c9299268381efe6af8d7546449090e22f6a0
AlexGraf <lexraf@list.ru>
2020-04-21 17:39:08 +0300

    Tests for login_page

---
+++ pages/locators.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    LOGIN_USERNAME = (By.CSS_SELECTOR, '[name="login-username"]')
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, '[name="login-password"]')
+    LOGIN_BUTTON = (By.CSS_SELECTOR, '[name="login_submit"]')
+    REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, '[name="login_submit"]')
+    REGISTRATION_PASSWORD_FIRST = (By.CSS_SELECTOR, '[name="registration-password1"]')
+    REGISTRATION_PASSWORD_SECOND = (By.CSS_SELECTOR, '[name="registration-password2"]')
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR, '[name="registration_submit"]')
